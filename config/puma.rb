83eb1abae9665476b3bed21b36e402054edbce51
kosugi <yuuuuuuuuuuuu0112@gmail.com>
2020-10-25 01:55:41 +0900

    nikaime

--- config/puma.rb
+++ config/puma.rb
@@ -35,3 +35,14 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+preload_app!
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+on_worker_boot do
+ ActiveRecord::Base.establish_connection
+end
